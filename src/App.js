78356794017cf4f5c37fbc889c7bca7ecaf155b9
TusharSachdeva <tusharsachdeva97@gmail.com>
2019-07-31 13:08:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Details from './Components/Details';
+import Cart from './Components/Cart';
+import Default from './Components/Default';
+import Modal from './Components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return(
+      <React.Fragment>
+      	<Navbar />
+      	<Switch>
+      	  <Route exact path='/' component={ProductList} />
+      	  <Route path ='/details' component={Details} />
+      	  <Route path ='/cart' component={Cart} />
+      	  <Route component = {Default} />
+      	</Switch>
+        <Modal />
+      </React.Fragment>
+    );
+ }
 }
 
 export default App;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
+import {ProductConsumer} from '../context';
+
+class ProductList extends Component{
+	constructor(){
+		super();
+			this.state = {
+				products: storeProducts
+			}
+	}
+  render() {
+    return(
+    	<React.Fragment>
+      	<div className ="py-5">
+          <div className ="container">
+            <Title name ="our" title ="products" />
+            <div className ="row">
+             <ProductConsumer>
+             	{(any)=>{
+             		return any.products.map(product => {
+                         return <Product key = {product.id}
+                         product={product}/>;
+             		})
+             	}}
+             </ProductConsumer>
+            </div>
+          </div>
+      	</div>
+      	</React.Fragment>
+    );
+ }
+}
+
+export default ProductList;
